ba032b776244aa0e98fdea22bccf44000157b296
aarcardini <alejandro.arcardini@hotmail.com>
2021-08-01 14:43:02 -0300

    intento php

--- enviar.php
+++ enviar.php
@@ -1,21 +1,26 @@
 <?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-if (isset($_POST['enviar'])) {
-    if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["telephone"]) && !empty($_POST["subject"]) && !empty($_POST["message"])) {
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $telephone = $_POST['telephone'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
-        $header = "From: alejandro@arar.uy" . "\r\n";
-        $header. = "Reply-To: alejandro@arar.uy" . "\r\n";
-        $header. = "X-Mailer: PHP/". phoversion();
-        $mail = @mail($email,$subject,$message,$header);
-        if ($mail) {
-            echo 'mensaje enviado correctamente';
-        }
-    }
-}
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "El asunto es: " . $subject . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
+$para = 'alejandro@arar.uy';
+$asunto = 'Mensaje de...';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
